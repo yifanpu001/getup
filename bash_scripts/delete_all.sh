49a1cc6eae125d53f7ea6004e55ef26e0df29618
Qihang Zhang <qihangz.work@gmail.com>
2024-02-08 21:58:38 -0800

    2024-02-08 update

--- bash_scripts/delete_all.sh
+++ bash_scripts/delete_all.sh
@@ -1,11 +1,11 @@
-rm -rf getup-core*
-rm -rf docs*
-rm -rf python_scripts*
-rm -rf scripts*
-rm -rf mkdocs.yml*
-rm -rf mkdocs_template.yml*
-rm -rf requirements.txt*
+# rm -rf getup-core*
+# rm -rf docs*
+# rm -rf python_scripts*
+# rm -rf scripts*
+# rm -rf mkdocs.yml*
+# rm -rf mkdocs_template.yml*
+# rm -rf requirements.txt*
 
 git add .
 git commit -m "$(date +%Y-%m-%d) update"
-git push
+git push origin gh-pages:gh-pages
--- bash_scripts/fetch_core.sh
+++ bash_scripts/fetch_core.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/Qihang-Zhang/getup-core.git
 
-ln -s getup-core/docs ./docs
-ln -s getup-core/python_scripts ./python_scripts
-ln -s getup-core/scripts ./scripts
-ln -s getup-core/mkdocs_template.yml ./mkdocs_template.yml
-ln -s getup-core/requirements.txt ./requirements.txt
+ln -s getup-core/* .
+# ln -s getup-core/docs ./docs
+# ln -s getup-core/python_scripts ./python_scripts
+# ln -s getup-core/scripts ./scripts
+# ln -s getup-core/mkdocs_template.yml ./mkdocs_template.yml
+# ln -s getup-core/requirements.txt ./requirements.txt
