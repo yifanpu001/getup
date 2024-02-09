043ca172f4266153e71264e52315d7ce0070fc90
Qihang Zhang <qihangz.work@gmail.com>
2024-02-08 22:07:06 -0800

    2024-02-08 update

--- bash_scripts/fetch_core.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/Qihang-Zhang/getup-core.git
-
-ln -s getup-core/* .
-# ln -s getup-core/docs ./docs
-# ln -s getup-core/python_scripts ./python_scripts
-# ln -s getup-core/scripts ./scripts
-# ln -s getup-core/mkdocs_template.yml ./mkdocs_template.yml
-# ln -s getup-core/requirements.txt ./requirements.txt
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+git clone https://github.com/Qihang-Zhang/getup-core.git
+
+ln -s getup-core/* .
+
+pip install -r requirements.txt
+
 python ./python_scripts/merge_mkdocs.py
 
 python ./python_scripts/generate_md.py \
@@ -7,3 +13,7 @@ python ./python_scripts/generate_md.py \
 --name Qihang
                       
 mkdocs gh-deploy --force
+
+git add .
+git commit -m "$(date +%Y-%m-%d) update"
+git push origin gh-pages:gh-pages
